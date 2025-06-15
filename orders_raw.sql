89df098430d1c104914a484d3fa524de7d2c5aa6
thhart-dbx <thhart-dbx>
2025-06-15 12:17:34 +0000

    Updated orders_raw to handle nulls

--- orders_raw.sql
+++ orders_raw.sql
@@ -6,7 +6,7 @@ as
 select 
   customer_id
   , order_number
-  , from_unixtime(order_datetime::bigint) as order_datetime
+  , from_unixtime(try_cast(order_datetime as bigint)) as order_datetime
 from read_files(
   '/databricks-datasets/retail-org/sales_orders'
   , format => 'json'
